43b61bdd8f8ae5504978d54a23c1d45c2853a27e
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2021-02-21 20:03:02 +0100

    Resnket koden litt, la til noen kommentarer og fjernet en del ubrukt kode

--- src/main.py
+++ src/main.py
@@ -33,6 +33,8 @@ from pygame import Vector2
 #from pygame.draw import rect
 
 from random import randint
+
+from pygame import key
 from paddle import Paddle   # Rekkerten
 from brick import Brick     # Brikkene
 from ball import Ball       # Ballen
@@ -116,102 +118,6 @@ def intersect_circles(circle_a_position, circle_a_radius,
         return None
 
 
-def example_code():
-    """ Example showing the use of the above code. """
-
-    screen_res = (640, 480)
-    pygame.init()
-
-    rectangle_a_position = Vector2(320, 320)
-    rectangle_a_size_x = rectangle_a_size_y = 128
-
-    rectangle_b_position = Vector2(250, 250)
-    rectangle_b_size_x = rectangle_b_size_y = 10
-
-    circle_a_position = Vector2(10, 10)
-    circle_a_radius = 6
-    circle_a_speed = Vector2(5, 5)
-
-    circle_b_position = Vector2(150, 150)
-    circle_b_radius = 10
-
-    screen = pygame.display.set_mode(screen_res)
-    clock = pygame.time.Clock()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                os.sys.exit()
-
-        pygame.draw.rect(screen, (0, 0, 0), ((0, 0), screen_res))
-        clock.tick(30)
-
-        x, y = pygame.mouse.get_pos()
-        circle_a_position = Vector2(x, y)
-
-        pygame.draw.rect(screen, (255, 255, 255),
-                         (rectangle_a_position.x,
-                         rectangle_a_position.y,
-                         rectangle_a_size_x,
-                         rectangle_a_size_y))
-
-        pygame.draw.rect(screen, (255, 255, 255),
-                         (rectangle_b_position.x,
-                         rectangle_b_position.y,
-                         rectangle_b_size_x,
-                         rectangle_b_size_y))
-
-        pygame.draw.circle(screen, (255, 255, 255),
-                           (int(circle_b_position.x),
-                           int(circle_b_position.y)),
-                           circle_b_radius)
-
-        pygame.draw.circle(screen, (255, 0, 0),
-                           (int(circle_a_position.x),
-                           int(circle_a_position.y)),
-                           circle_a_radius)
-
-        def draw_vec_from_ball(vec, col):
-            """ Draw a vector from the mouse controlled circle. """
-            pygame.draw.line(screen, col,
-                             (circle_a_position.x, circle_a_position.y),
-                             (circle_a_position.x + vec.x * 20,
-                              circle_a_position.y + vec.y * 20), 3)
-
-        draw_vec_from_ball(circle_a_speed, (255, 255, 0))
-
-        impulse = intersect_rectangle_circle(rectangle_a_position,
-                                             rectangle_a_size_x,
-                                             rectangle_a_size_y,
-                                             circle_a_position,
-                                             circle_a_radius,
-                                             circle_a_speed)
-        if impulse:
-            draw_vec_from_ball(impulse, (0, 255, 255))
-
-        impulse = intersect_rectangle_circle(rectangle_b_position,
-                                             rectangle_b_size_x,
-                                             rectangle_b_size_y,
-                                             circle_a_position,
-                                             circle_a_radius,
-                                             circle_a_speed)
-        if impulse:
-            draw_vec_from_ball(impulse, (0, 255, 255))
-
-        impulse = intersect_circles(circle_a_position, circle_a_radius,
-                                    circle_b_position, circle_b_radius)
-        if impulse:
-            draw_vec_from_ball(impulse, (0, 255, 255))
-
-        pygame.display.update()
-
-
-def example2():
-    V1 = Vector2(300, 300)
-    V2 = Vector2(100, 100)
-
-    V3 = V1 + V2
-
-    print(V3)
 
 
 def create_font(text, font_family="Arial", size=48, color=(255, 255, 255), bold=False, italic=False):
@@ -223,7 +129,7 @@ def create_font(text, font_family="Arial", size=48, color=(255, 255, 255), bold=
 
 
 
-def my_code():
+def game():
     pygame.init()
 
     score = 0
@@ -287,58 +193,19 @@ def my_code():
         top_offset += tile_height + margin
 
 
-    playing = True
+    playing = True              # Game-loop vaiabelen, holder seg True mens spillet pågår
+                                # Blir satt til False når score = 0 eller spilleren avslutter
 
     clock = pygame.time.Clock()
 
-    # `initial_open' er en variabel som tyder på om du nettopp åpnet spillet
-    # denne brukes for å lage en liten "splash screen" før selve spillet starter
-    initial_open = True
+    started = False             # Variabel som tyder på om spillet er startet eller ikke
 
-    while initial_open:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                initial_open = False
-                playing = False
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            initial_open = False
-            ball.rect.x = (screen.get_width() - 2*ball.radius) // 2
-            ball.rect.y = paddle.rect.y - ball.radius
-            ball.velocity = [randint(4, 8), randint(-8, 8)]
-
-
-        screen.fill((0,0,0))    # Velger bakgrunnsfargen
-
-        start_text, start_box = create_font("Press SPACE to start!", size=72, color=WHITE)
-        sb_center_x, sb_center_y = screen.get_width() // 2, screen.get_height() //2
-        start_box = start_text.get_rect(center=(sb_center_x, sb_center_y))
-        screen.blit(start_text, start_box)
-
-
-        """
-        # Game over text, vertically and horizontally centered
-        gameover_text, gameover_box = create_font("GAME OVER", "Arial", 128, RED)
-        center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
-        gameover_box = gameover_text.get_rect(center=(center_x, center_y))
-
-        # Restart text, only horizontally centered, and biased to the bottom
-        restart_text, restart_box = create_font("Press SPACE to restart")
-        restart_box = int((screen.get_width() - restart_text.get_width()) // 2), screen.get_height() - restart_text.get_height()
-        screen.blit(restart_text, restart_box)
-
-        screen.blit(gameover_text, gameover_box)
-        """
-
-        pygame.display.flip()
-        clock.tick(60)
-
+    bounce_factor = 10          # Faktor som multipliseres med normalvektoren som gjengis av kollisjonsfunksjonen
 
     while playing:
         # Event handling
-        # Den ene sjekker for om spilleren trykker på krysset i hjørnet
-        # den andre sjekker for om spilleren trøkker Q-tasten
+        # Håndterer exit-contitions; ved et trykk på krysset i hjørnet av vinduet,
+        # eller ved å trykke q-tasten
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 playing = False                 # Setter er flagg slik at vi kan hoppe ut av loopen
@@ -346,7 +213,22 @@ def my_code():
                 if event.key == pygame.K_q:     # Hvis Q-trykkes avsluttes spillet
                     playing = False
 
-        # Flytter rekkerten med piltastene istedenfor musepekeren
+        # Fyller skjermen med en heldekkende farge, klar til å tegnes på
+        screen.fill(BLACK)
+
+        if not started:
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[pygame.K_SPACE]:
+                started = True
+
+            start_text, start_box = create_font("Press SPACE to start!", size=72, color=WHITE)
+            sb_center_x, sb_center_y = screen.get_width() // 2, screen.get_height() //2
+            start_box = start_text.get_rect(center=(sb_center_x, sb_center_y))
+            screen.blit(start_text, start_box)
+
+
+        # Event-handling for bruker-input
+        # Flytter rekkerten til venstre eller høyre avhengig av piltasten som trykkes
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             paddle.move_left(15)
@@ -354,26 +236,21 @@ def my_code():
             paddle.move_right(15)
 
         brick_list = bricks.sprites()
-        for brick in brick_list:
-            # Sjekk for kollisjon mellom ball og brick
+        for brick in brick_list:        # Looper gjennom alle elementer i bricks-gruppen og sjekker for kollisjon
+            # Kollisjonshåndtering
             impulse = intersect_rectangle_circle(Vector2(brick.rect.x, brick.rect.y), brick.rect.width, brick.rect.height, Vector2(ball.rect.x, ball.rect.y), ball.radius, Vector2(ball.velocity))
             if impulse:
-                print("Ball has hit a brick!")
-                ball.velocity[0] = impulse.x*10
-                ball.velocity[1] = impulse.y*10
-                #bricks.remove(brick)
-                #sprites.remove(brick)
-                brick.kill()            # Denne fjerner en brick fra sprites gruppa
-                #bricks.update()        # Disse visste seg å være unødvendig
-                #sprites.update()
-                score += 1
+                ball.velocity[0] = impulse.x*bounce_factor
+                ball.velocity[1] = impulse.y*bounce_factor
+                brick.kill()            # Brick objektet som har blitt kollidert med fjernes fra alle sprite-grupper
+                                        # og blir dermed fjernet fra brettet
+                score += 1              # Inkrementer score med 1 da bruker knuste en brikke
         
         # Sjekk for kollisjon mellom ball og rekkert
         impulse = intersect_rectangle_circle(Vector2(paddle.rect.x, paddle.rect.y), paddle.rect.width, paddle.rect.height, Vector2(ball.rect.x, ball.rect.y), ball.radius, Vector2(ball.velocity))
         if impulse:
-            print("Hit the paddle!")
-            ball.velocity[0] = impulse.x*10
-            ball.velocity[1] = impulse.y*10
+            ball.velocity[0] = impulse.x*bounce_factor
+            ball.velocity[1] = impulse.y*bounce_factor
         
         
 
@@ -393,8 +270,7 @@ def my_code():
             # hvis du treffer taket har du vel vunnet, right? Break out?
 
 
-        # Fyller skjermen med en heldekkende farge, klar til å tegnes på
-        screen.fill(BLACK)
+        
 
         # Linjen øverst på skjermen som viser poengsum og antall liv igjen
         pygame.draw.line(screen, WHITE, [0, 38], [800, 38], 2)
@@ -451,5 +327,5 @@ def game_over(screen):
 
 
 if __name__ == '__main__':
-    my_code()
+    game()
     pygame.quit()
